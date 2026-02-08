eea629134ec2e3abaaecb42c451b09113b95166f
Akash Singh <94743735+akash0425@users.noreply.github.com>
2026-02-09 00:57:20 +0530

    Time: 409 ms (56.3%), Space: 0B (100%) - LeetHub

--- 0619-biggest-single-number/0619-biggest-single-number.sql
+++ 0619-biggest-single-number/0619-biggest-single-number.sql
@@ -1,6 +1,13 @@
 # Write your MySQL query statement below
-select
-case when count(num)=1 then num else null end as num
+-- select
+-- case when count(num)=1 then num else null end as num
+-- from MyNumbers
+-- group by num
+-- order by num desc limit 1
+
+select max(num) as num from
+(select num,count(num) as freq
 from MyNumbers
 group by num
-order by num desc limit 1
+) as counted
+where freq = 1
