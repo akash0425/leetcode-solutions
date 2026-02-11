1121730dbe0ee5a04b24daa020b609b5b82b8a3f
Akash Singh <94743735+akash0425@users.noreply.github.com>
2026-02-11 23:44:13 +0530

    Time: 527 ms (55.8%), Space: 0B (100%) - LeetHub

--- 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
+++ 1789-primary-department-for-each-employee/1789-primary-department-for-each-employee.sql
@@ -1,9 +1,15 @@
 # Write your MySQL query statement below
 
-with temp as (
-select * , count(primary_flag)over(partition by employee_id ) as cnt
-from Employee)
+-- with temp as (
+-- select * , count(primary_flag)over(partition by employee_id ) as cnt
+-- from Employee)
 
-select employee_id , department_id
-from temp
-where (cnt = 1) or (cnt >1 and primary_flag = 'Y')
+-- select employee_id , department_id
+-- from temp
+-- where (cnt = 1) or (cnt >1 and primary_flag = 'Y')
+
+with tempp as(
+select * , row_number() over(partition by employee_id order by primary_flag) as rankk
+from Employee)
+select employee_id,department_id from tempp
+where rankk=1;
